a8fead7d17d1a6aeaa0839e245c78b99d5e48aa2
emanuel2718 <eramirez2718@gmail.com>
2021-05-19 11:04:39 -0400

    add function annotations to reddit_client

--- myreddit-dl/reddit_client.py
+++ myreddit-dl/reddit_client.py
@@ -3,7 +3,7 @@ import praw
 
 class RedditClient:
 
-    def __init__(self, args):
+    def __init__(self, args: dict) -> None:
         self.args = args
         self.config = configparser.ConfigParser()
         self.config.read('config.ini')
@@ -11,11 +11,10 @@ class RedditClient:
         # NOTE: self.user is None if config file is empty
         self.reddit_instance = self.build_reddit_instance()
         self.user = self.reddit_instance.user.me()
-
         self.check_if_valid_user()
 
 
-    def build_reddit_instance(self):
+    def build_reddit_instance(self) -> praw.Reddit:
         return praw.Reddit(
             user_agent='MyReddit-dl',
             client_id=self.config['REDDIT']['client_id'],
@@ -23,26 +22,26 @@ class RedditClient:
             username=self.config['REDDIT']['username'],
             password=self.config['REDDIT']['password'])
 
-    def get_user(self):
-        return self.user
+    def get_user(self) -> str:
+        return str(self.user)
 
-    def get_args(self):
+    def get_args(self) -> dict:
         return self.args
 
-    def check_if_valid_user(self):
+    def check_if_valid_user(self) -> None:
         if self.user:
             return
         util.print_error(util.USER_NOT_FOUND)
         exit(1)
 
-    def get_user_upvotes(self):
+    def get_user_upvotes(self) -> praw.models.listing.generator.ListingGenerator:
         ''' Returns a ListingGenerator of the user upvoted posts if the
             user asked for the saved files with the (-U --upvote) flag.
             Otherwise, return None
         '''
         return self.user.upvoted(limit=None) if self.args['upvote'] else None
 
-    def get_user_saves(self):
+    def get_user_saves(self) -> praw.models.listing.generator.ListingGenerator:
         ''' Returns a ListingGenerator of the user saved posts if the
             user asked for the saved files with the (-S --saved) flag.
             Otherwise, return None

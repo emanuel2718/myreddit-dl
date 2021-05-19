b79c1007260b70b39680654cdbdbc2cfaf892c61
emanuel2718 <eramirez2718@gmail.com>
2021-05-19 11:00:06 -0400

    refactor file_handler into a class

--- myreddit-dl/file_handler.py
+++ myreddit-dl/file_handler.py
@@ -2,38 +2,47 @@ import cli
 import os
 import re
 
+SUPPORTED_MEDIA_FORMATS = ('.jpg', '.png', '.jpeg', 'gif', 'gifv')
 
-def get_path(args, username, sub):
-    # by test_dir/subreddit
-    if args['debug']:
-        return f'{os.getcwd()}{os.sep}test_dir{os.sep}{sub}{os.sep}'
+class FileHandler:
+    def __init__(self, args, username, item) -> None:
+        self.args = args
+        self.username = username
+        self.sub = item.subreddit
+        self.url = item.url
+        self.author = item.author
 
-    # by media/subreddit
-    if args['subreddit']:
-        return f'{os.getcwd()}{os.sep}media{os.sep}{sub}{os.sep}'
+    def get_path(self) -> str:
+        # by test_dir/subreddit
+        if self.args['debug']:
+            return f'{os.getcwd()}{os.sep}test_dir{os.sep}{self.sub}{os.sep}'
 
-    # by media/username
-    else:
-        return f'{os.getcwd()}{os.sep}media{os.sep}{username}{os.sep}'
+        # by media/subreddit
+        if self.args['subreddit']:
+            return f'{os.getcwd()}{os.sep}media{os.sep}{self.sub}{os.sep}'
 
-def is_media(filename):
-    if filename.endswith(('.jpg', '.png', '.jpeg', 'gif', 'gifv')):
-        return True
-    return False
+        # by media/username
+        else:
+            return f'{os.getcwd()}{os.sep}media{os.sep}{self.username}{os.sep}'
 
-def get_filename(args, user, item):
-    ''' Converts urls of any format like:
-            -example: https://i.imgur.com/bMstO8O.jpg
-        and extracts only the bMst080.jpg portion of it as the filename.
+    def is_media(self) -> bool:
+        if self.get_filename().endswith(SUPPORTED_MEDIA_FORMATS):
+            return True
+        return False
 
-        If the -user flag was given then append the username before the filename.
-            -example: random_bMst080.jpg
-    '''
-    path = get_path(args, user, item.subreddit)
-    if args['user']:
-        return path + str(item.author) + '_' + \
-            re.search('(?s:.*)\\w/(.*)', item.url).group(1)
-    return path + re.search('(?s:.*)\\w/(.*)', item.url).group(1)
+    def get_filename(self) -> str:
+        ''' Converts urls of any format like:
+                -example: https://i.imgur.com/bMstO8O.jpg
+            and extracts only the bMst080.jpg portion of it as the filename.
+
+            If the -user flag was given then append the username before the filename.
+                -example: random_bMst080.jpg
+        '''
+        path = self.get_path()
+        if self.args['user']:
+            return path + self.author + '_' + \
+                re.search('(?s:.*)\\w/(.*)', self.url).group(1)
+        return path + re.search('(?s:.*)\\w/(.*)', self.url).group(1)
 
 
 if __name__ == '__main__':

0d9706052d273c403360a8b6af8af484f1b72572
emanuel2718 <eramirez2718@gmail.com>
2021-05-20 14:09:16 -0400

    added gif support and refactored downloader
    Added .gif and .gifv support changing the extensions to .mp4
    Added a class variable called `self._item` that will make it so
    that we don't need to pass the entire reddit instance everytime in
    `downloader.py`. Just copy it once during the in `_iterate_items` for
    each valid item instead of copying up to 10 times per item on the worst
    case like it was before this change.
    
    Also refactored the media fetching function calls for each domain to
    it's own function to avoid having too much conditionals on
    `_iterate_items` function.

--- myreddit-dl/downloader.py
+++ myreddit-dl/downloader.py
@@ -4,7 +4,6 @@ import pprint
 import praw
 import re
 import requests
-from bs4 import BeautifulSoup
 from file_handler import FileHandler
 
 
@@ -18,74 +17,105 @@ class Downloader:
         self.subreddit_list = self.args['subreddit']
         self.by_user = self.args['user']
         self.limit = self.args['limit']
-        self.valid_domains = self._get_valid_domains()
+        self.valid_domains = self._get_valid_sfw_domains()
         self.valid_nsfw_domains = self._get_valid_nsfw_domains()
         self.download_counter = 0
+        self._item = None
         self.start()
 
-    def _is_gallery_item(self, url):
-        if url.startswith('https://www.reddit.com/gallery/'):
+    def _is_gallery_item(self) -> bool:
+        if self._item.url.startswith('https://www.reddit.com/gallery/'):
             return True
         return False
 
-    def _is_valid_domain(self, domain: str) -> bool:
-        return True if domain in self.valid_domains else False
+    def _is_valid_domain(self) -> bool:
+        return True if self._item.domain in self.valid_domains else False
 
-    def _get_valid_domains(self) -> set:
+    def _get_valid_sfw_domains(self) -> set:
         return {'v.redd.it', 'i.redd.it', 'i.imgur.com', 'gfycat.com', 'streamable.com'}
 
     def _get_valid_nsfw_domains(self) -> set:
         return {'redgifs.com', 'erome.com'}
 
-    def _get_vreddit_url(self, item):
+    def _get_vreddit_url(self):
         ''' For the https://v.redd.it posts'''
-        if item.media is None:
-            return item.crosspost_parent_list[0]['media']['reddit_video']['fallback_url']
+        if self._item.media is None:
+            return self._item.crosspost_parent_list[0]['media']['reddit_video']['fallback_url']
         else:
-            return item.media['reddit_video']['fallback_url']
+            return self._item.media['reddit_video']['fallback_url']
 
-    def _get_gyfcat_url(self, item) -> str or None:
+    def _get_gyfcat_url(self) -> str or None:
         try:
-            return item.preview['reddit_video_preview']['fallback_url']
+            return self._item.preview['reddit_video_preview']['fallback_url']
         except BaseException:
             return None
 
-    def _get_redgifs_url(self, item) -> str or None:
+    def _get_redgifs_url(self) -> str or None:
         try:
             return item.preview['reddit_video_preview']['fallback_url']
         except BaseException:
             pass
 
         # need to extract the video link through html requests
-        response = requests.get(item.url).text
+        response = requests.get(self._item.url).text
         urls = re.findall(r'https?://[^\s<>"]+|www\.[^\s<>"]+', str(response))
         for url in urls:
             if url.endswith('.mp4'):
                 return url
         return None
 
-    def _get_streamable_url(self, item):
+    def _get_streamable_url(self) -> str or None:
         try:
-            html = item.media['oembed']['html']
+            html = self._item.media['oembed']['html']
             url = re.findall(r'https?://[^\s<>"]+|www\.[^\s<>"]+', str(html))
             return url + '.mp4'
         except BaseException:
             return None
+
+    def _get_mp4_from_gifs(self) -> str:
+        ''' Replace .gifv and .gif extensions with .mp4 extension.'''
+        return self._item.url.replace('gifv', 'mp4').replace('gif', 'mp4')
+
+    def get_media_url_from_domain(self) -> str:
+        if self._item.domain == 'v.redd.it':
+            media_url = self._get_vreddit_url()
+        elif self._item.domain == 'gfycat.com':
+            media_url = self._get_gyfcat_url()
+        elif self._item.domain == 'redgifs.com':
+            media_url = self._get_redgifs_url()
+        elif self._item.domain == 'streamable.com':
+            media_url = self._get_streamable_url()
+        elif self._item.url.endswith(('gif', 'gifv')):
+            media_url = self._get_mp4_from_gifs()
+        else:
+            media_url = self._item.url
+        # TODO: Handle Gallery case: https://www.reddit.com/gallery/ltvpmj
+        #       Some galleries contain more than 10 photos or videos. What to do?
+        #       Ignore the reddit.com/r/Subreddit/comments posts.
+
+        # TODO: Handle Imgur gallery case: https://imgur.com/a/oOnxxk3/
+        #       There are cases where the imgur.com/ ends with .gifv and or png
+        #       those cases should be easy to handle. The problem lies in the galleries.
+        #       Maybe requests to the rescue?
+        #
+        # TODO: Handle: https://www.twitch.tv/mande/clip/ImpossibleHilariousOrcaCurseLit-QsfyEix6y5zaCMMN?filter=clips&range=7d&sort=time
+        #       Adding .mp4 to the end might solve it. Havent' tried with GET
+
+        # TODO: Handle: https://clips.twitch.tv/SeductiveMagnificentNikudonRaccAttack-dvnHYTkEyOvqaExq
+        #       Adding .mp4 makes the clip go missing
 
     def _iterate_items(self, items):
         for item in items:
             media_url = None
             # Avoid saved comments posts
             if not isinstance(item, praw.models.reddit.comment.Comment):
-                if self._is_valid_domain(item.domain):
-                    if item.domain == 'v.redd.it':
-                        media_url = self._get_vreddit_url(item)
-                    if item.domain == 'gfycat.com':
-                        media_url = self._get_gyfcat_url(item)
-                    if item.domain == 'redgifs.com':
-                        media_url = self._get_redgifs_url(item)
-                    if item.domain == 'streamable.com':
-                        media_url = self._get_streamable_url(item)
+                self._item = item
+                if self._is_valid_domain():
+                    media_url = self.get_media_url_from_domain()
+                else:
+                    print(self._item.url)
+
+                
 
     def start(self):
         if self.args['nsfw']:

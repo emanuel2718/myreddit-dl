118c6be2d4abc29efc2a86e589ac0f745ebfa1c6
emanuel2718 <eramirez2718@gmail.com>
2021-05-20 19:03:34 -0400

    add property decorators in reddit_client

--- myreddit-dl/reddit_client.py
+++ myreddit-dl/reddit_client.py
@@ -3,14 +3,14 @@ import praw
 
 class RedditClient:
 
-    def __init__(self, args: dict) -> None:
-        self.args = args
+    def __init__(self, arg_dict: dict) -> None:
+        self.arg_dict = arg_dict
         self.config = configparser.ConfigParser()
         self.config.read('config.ini')
 
         # NOTE: self.user is None if config file is empty
         self.reddit_instance = self.build_reddit_instance()
-        self.user = self.reddit_instance.user.me()
+        self.username = self.reddit_instance.user.me()
         self.check_if_valid_user()
 
 
@@ -22,31 +22,36 @@ class RedditClient:
             username=self.config['REDDIT']['username'],
             password=self.config['REDDIT']['password'])
 
-    def get_user(self) -> str:
-        return str(self.user)
-
-    def get_args(self) -> dict:
-        return self.args
-
     def check_if_valid_user(self) -> None:
-        if self.user:
+        if self.username:
             return
         util.print_error(util.USER_NOT_FOUND)
         exit(1)
 
-    def get_user_upvotes(self) -> praw.models.listing.generator.ListingGenerator:
+    @property
+    def user(self) -> str:
+        return str(self.username)
+
+    @property
+    def args(self) -> dict:
+        return self.arg_dict
+
+
+    @property
+    def upvotes(self) -> praw.models.listing.generator.ListingGenerator:
         ''' Returns a ListingGenerator of the user upvoted posts if the
             user asked for the saved files with the (-U --upvote) flag.
             Otherwise, return None
         '''
-        return self.user.upvoted(limit=None) if self.args['upvote'] else None
+        return self.username.upvoted(limit=None) if self.args['upvote'] else None
 
-    def get_user_saves(self) -> praw.models.listing.generator.ListingGenerator:
+    @property
+    def saves(self) -> praw.models.listing.generator.ListingGenerator:
         ''' Returns a ListingGenerator of the user saved posts if the
             user asked for the saved files with the (-S --saved) flag.
             Otherwise, return None
         '''
-        return self.user.saved(limit=None) if self.args['saved'] else None
+        return self.username.saved(limit=None) if self.args['saved'] else None
 
 if __name__ == '__main__':
     cli.print_warning(cli.DONT_RUN_THIS_FILE)

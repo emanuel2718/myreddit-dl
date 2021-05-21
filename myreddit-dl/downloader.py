c0111bda6d254bbda3a46c1b1737f69b1e2c5691
emanuel2718 <eramirez2718@gmail.com>
2021-05-20 22:34:25 -0400

    refactor and added support for reddit and imgur galleries

--- myreddit-dl/downloader.py
+++ myreddit-dl/downloader.py
@@ -14,8 +14,12 @@ class Downloader:
         self.valid_domains = utils.SFW_DOMAINS
         self.download_counter = 0
 
-        self._item = None # current upvoted or saved post we are looking at.
+        self._item = None  # current upvoted or saved post we are looking at.
         self.start()
+
+    @property
+    def __print_item(self):
+        pprint.pprint(vars(self._item))
 
     @property
     def sfw_domains(self) -> set:
@@ -33,23 +37,26 @@ class Downloader:
     def _is_valid_domain(self) -> bool:
         return True if self._item.domain in self.valid_domains else False
 
-
-    def _get_vreddit_url(self):
+    @property
+    def _vreddit_url(self) -> list[str]:
         ''' For the https://v.redd.it posts'''
         if self._item.media is None:
-            return self._item.crosspost_parent_list[0]['media']['reddit_video']['fallback_url']
+            return [self._item.crosspost_parent_list[0]
+                    ['media']['reddit_video']['fallback_url']]
         else:
-            return self._item.media['reddit_video']['fallback_url']
+            return [self._item.media['reddit_video']['fallback_url']]
 
-    def _get_gyfcat_url(self) -> str or None:
+    @property
+    def _gyfcat_url(self) -> list[str]:
         try:
-            return self._item.preview['reddit_video_preview']['fallback_url']
+            return [self._item.preview['reddit_video_preview']['fallback_url']]
         except BaseException:
-            return None
+            return []
 
-    def _get_redgifs_url(self) -> str or None:
+    @property
+    def _redgifs_url(self) -> list[str]:
         try:
-            return item.preview['reddit_video_preview']['fallback_url']
+            return [item.preview['reddit_video_preview']['fallback_url']]
         except BaseException:
             pass
 
@@ -58,38 +65,61 @@ class Downloader:
         urls = re.findall(r'https?://[^\s<>"]+|www\.[^\s<>"]+', str(response))
         for url in urls:
             if url.endswith('.mp4'):
-                return url
-        return None
+                return [url]
+        return []
 
-    def _get_streamable_url(self) -> str or None:
+    @property
+    def _streamable_url(self) -> list[str]:
         try:
             html = self._item.media['oembed']['html']
             url = re.findall(r'https?://[^\s<>"]+|www\.[^\s<>"]+', str(html))
-            return url + '.mp4'
+            return [url + '.mp4']
         except BaseException:
-            return None
+            return []
 
-    def _get_mp4_from_gifs(self) -> str:
+    @property
+    def _reddit_gallery_url(self) -> list[(str, str), ...]:
+        ''' Returns a list of dual strings tuples. The tuples contains the url and the
+            extension of the image as image/jpg or image/png.
+        '''
+        try:
+            metadata = self._item.media_metadata.values()
+            return [(i['s']['u'], i['m']) for i in metadata if i['e'] == 'Image']
+        except BaseException:
+            return []  # deleted post
+
+    @property
+    def _imgur_gallery_url(self) -> list[str, ...]:
+        try:
+            return self._item.preview['images'][0]['source']['url']
+        except BaseException:
+            return []
+
+    @property
+    def _mp4_url_from_gif_url(self) -> list[str]:
         ''' Replace .gifv and .gif extensions with .mp4 extension.'''
-        return self._item.url.replace('gifv', 'mp4').replace('gif', 'mp4')
+        return [self._item.url.replace('gifv', 'mp4').replace('gif', 'mp4')]
 
-    def get_media_url_from_domain(self) -> str:
+    def get_media_url(self) -> list[str, ...]:
+        # TODO: maybe make this into some kind of for loop through
+        #       the valid domains?
         if self._item.domain == 'v.redd.it':
-            media_url = self._get_vreddit_url()
+            media_url = self._vreddit_url
         elif self._item.domain == 'gfycat.com':
-            media_url = self._get_gyfcat_url()
+            media_url = self._gyfcat_url
         elif self._item.domain == 'redgifs.com':
-            media_url = self._get_redgifs_url()
+            media_url = self._redgifs_url
         elif self._item.domain == 'streamable.com':
-            media_url = self._get_streamable_url()
+            media_url = self._streamable_url
+        elif self._item.domain == 'imgur.com' and not self._item.url.endswith(('jpg', 'png')):
+            media_url = self._imgur_gallery_url
+        elif self._item.url.startswith(utils.REDDIT_GALLERY_URL):
+            media_url = self._reddit_gallery_url
         elif self._item.url.endswith(('gif', 'gifv')):
-            media_url = self._get_mp4_from_gifs()
+            media_url = self._mp4_url_from_gif_url
         else:
-            media_url = self._item.url
-        # TODO: Handle Gallery case: https://www.reddit.com/gallery/ltvpmj
-        #       Some galleries contain more than 10 photos or videos. What to do?
-        #       Ignore the reddit.com/r/Subreddit/comments posts.
-
+            media_url = [self._item.url]  # all the png and jpg ready for download
+        print(media_url)
         # TODO: Handle Imgur gallery case: https://imgur.com/a/oOnxxk3/
         #       There are cases where the imgur.com/ ends with .gifv and or png
         #       those cases should be easy to handle. The problem lies in the galleries.
@@ -103,16 +133,16 @@ class Downloader:
 
     def _iterate_items(self, items):
         for item in items:
-            media_url = None
+            media_url = []
             # Avoid saved comments posts
             if not isinstance(item, praw.models.reddit.comment.Comment):
                 self._item = item
                 if self._is_valid_domain():
-                    media_url = self.get_media_url_from_domain()
+                    media_url = self.get_media_url()
                 else:
-                    print(self._item.url)
-
-                
+                    pass
+                    #print(f'Domain: {self._item.domain} || Url: {self._item.url}')
+                    # print(self._item.url)
 
     def start(self):
         if self.client.args['nsfw']:

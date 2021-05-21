c4f5916f770715c676874d3356a920d8e501b3ab
emanuel2718 <eramirez2718@gmail.com>
2021-05-20 22:33:40 -0400

    add an utility helper file

---
+++ myreddit-dl/utils.py
@@ -0,0 +1,4 @@
+REDDIT_GALLERY_URL = 'https://www.reddit.com/gallery/'
+NSFW_DOMAINS = {'redgifs.com', 'erome.com'}
+SFW_DOMAINS = {'v.redd.it', 'i.redd.it', 'i.imgur.com',
+               'gfycat.com', 'streamable.com', 'reddit.com', 'imgur.com'}

fe4bd44f3dd6815a3510b642795bbd2b2c2368cf
emanuel2718 <eramirez2718@gmail.com>
2021-05-23 00:22:44 -0400

    refactor post validation on downloader

--- myreddit-dl/downloader.py
+++ myreddit-dl/downloader.py
@@ -9,7 +9,7 @@ from file_handler import FileHandler
 
 
 class Downloader:
-    def __init__(self, client) -> None:
+    def __init__(self, client: 'RedditClient') -> None:
         self.client = client
         self.valid_domains = utils.SFW_DOMAINS
         self.download_counter = 0
@@ -17,6 +17,10 @@ class Downloader:
         self._item = None  # current upvoted or saved post we are looking at.
         self.media_url = None
         self.start()
+
+    @property
+    def __print_item(self):
+        pprint.pprint(vars(self._item))
 
     @property
     def item(self):
@@ -39,17 +43,12 @@ class Downloader:
         return self.media_url
 
     @property
-    def __print_item(self):
-        pprint.pprint(vars(self._item))
-
-    @property
     def sfw_domains(self) -> set:
         return utils.SFW_DOMAINS
 
     @property
     def nsfw_domains(self) -> set:
         return utils.NSFW_DOMAINS
-
 
     @property
     def _vreddit_url(self) -> str:
@@ -110,14 +109,39 @@ class Downloader:
         ''' Replace .gifv and .gif extensions with .mp4 extension.'''
         return self._item.url.replace('gifv', 'mp4').replace('gif', 'mp4')
 
-
-    def _is_gallery_item(self) -> bool:
-        if self._item.url.startswith(utils.REDDIT_GALLERY_DOMAIN):
+    @property
+    def _is_comment(self) -> bool:
+        if isinstance(self._item, praw.models.reddit.comment.Comment):
             return True
         return False
 
+    @property
     def _is_valid_domain(self) -> bool:
         return True if self._item.domain in self.valid_domains else False
+
+    @property
+    def _is_valid_subreddit(self) -> bool:
+        ''' Returns True if any of the asked subreddits with the -sub flag
+            matches the current item subreddit.
+
+            Also returns True if the user didn't specify any subreddit, as
+            then it is implied that the user wants all the posts.
+        '''
+        if self.client.args['subreddit'] is None:
+            return True
+        return self.subreddit in self.client.args['subreddit']
+
+    def _is_valid_post(self) -> bool:
+        # TODO: think about a more clean way to handle this.
+        if self._item is None:
+            return False
+        if self._is_comment:
+            return False
+        if not self._is_valid_subreddit:
+            return False
+        if not self._is_valid_domain:
+            return False
+        return True
 
     def get_media_url(self) -> list[str, ...]:
         # TODO: maybe make this into some kind of for loop through
@@ -140,36 +164,19 @@ class Downloader:
             media_url = self._item.url  # all the png and jpg ready for download
 
         return media_url
-        # TODO: Handle Imgur gallery case: https://imgur.com/a/oOnxxk3/
-        #       There are cases where the imgur.com/ ends with .gifv and or png
-        #       those cases should be easy to handle. The problem lies in the galleries.
-        #       Maybe requests to the rescue?
-        #
         # TODO: Handle: https://www.twitch.tv/mande/clip/ImpossibleHilariousOrcaCurseLit-QsfyEix6y5zaCMMN?filter=clips&range=7d&sort=time
         #       Adding .mp4 to the end might solve it. Havent' tried with GET
 
         # TODO: Handle: https://clips.twitch.tv/SeductiveMagnificentNikudonRaccAttack-dvnHYTkEyOvqaExq
         #       Adding .mp4 makes the clip go missing
 
-    def is_item_from_valid_subreddit(self) -> bool:
-        ''' Returns True if any of the asked subreddits with the -sub flag
-            matches the current item subreddit.
-
-            Also returns True if the user didn't specify any subreddit, as
-            then it is implied that the user wants all the posts.
-        '''
-        if self.client.args['subreddit'] is None:
+    def download_limit_reached(self) -> bool:
+        if self.args['limit'] and self.download_counter >= self.args['limit']:
+            exit(1)
             return True
-        return self.subreddit in self.client.args['subreddit']
-
-    def download_limit_reached(self):
-        if self.args['limit']:
-            if self.download_counter >= self.args['limit']:
-                exit(1)
-                return True
         return False
 
-    def __write__(self, path):
+    def __write__(self, path) -> None:
         # This are gallery posts (multiple media per post)
         if isinstance(path, list):
             for p in path:
@@ -189,39 +196,25 @@ class Downloader:
             except BaseException:
                 utils.print_failed(f'Adding file : {path}')
 
-
-    def download(self, base_path, abs_path):
+    def download(self, base_path: str, abs_path: str) -> None:
         if self.download_limit_reached():
             return
-
-
         if not os.path.exists(base_path):
             os.makedirs(base_path)
-
-        #r = requests.get(self.item.url)
-
         self.__write__(abs_path)
         self.download_counter += 1
 
-
-    def _iterate_items(self, items):
+    def _iterate_items(self, items) -> None:
         for item in items:
-            #media_url = None
             self._item = item
-            if self._item and self.is_item_from_valid_subreddit():
-                # Avoid saved comments posts
-                # TODO: Check if this check can be done with @self._is_valid_domain
-                if not isinstance(item, praw.models.reddit.comment.Comment):
-                    # can be from a valid subreddit but not a media item
-                    if self._is_valid_domain():
-                        self.media_url = self.get_media_url()
-                        handler = FileHandler(self)
-                        base_path = handler.base_path
-                        absolute_path = handler.absolute_path
-                        #path = handler.get_path()
-                        self.download(base_path, absolute_path)
+            if self._is_valid_post():
+                self.media_url = self.get_media_url()
+                handler = FileHandler(self)
+                base_path = handler.base_path
+                absolute_path = handler.absolute_path
+                self.download(base_path, absolute_path)
 
-    def start(self):
+    def start(self) -> None:
         if self.client.args['nsfw']:
             self.valid_domains = self.sfw_domains.union(self.nsfw_domains)
         if self.client.args['upvote'] and not self.client.args['saved']:
--- myreddit-dl/file_handler.py
+++ myreddit-dl/file_handler.py
@@ -9,12 +9,12 @@ SEP = os.sep
 
 
 class FileHandler():
-    def __init__(self, cls):
+    def __init__(self, cls: 'Downloader') -> None:
         self.cls = cls
         self.media_url = self.cls.curr_media_url if self.cls.curr_media_url else ''
 
     @property
-    def abs_path_list(self):
+    def abs_path_list(self) -> list[str, str]:
         count = 0
         filenames = []
         for url in self.media_url:
@@ -23,7 +23,7 @@ class FileHandler():
         return filenames
 
     @property
-    def base_path(self):
+    def base_path(self) -> str:
         # TODO: Eventually we want path resolution from path given with --path flag
         if self.cls.args['debug']:
             return (CURRENT_DIR + 'test_dir' + SEP + self.cls.user + SEP
@@ -35,20 +35,17 @@ class FileHandler():
         if isinstance(self.media_url, list):
             # TODO: Why is this adding double quotes to the path?
             return self.abs_path_list
-            #return self.base_path + self.filenames_from_list
+            # return self.base_path + self.filenames_from_list
         return self.base_path + self.get_filename(self.media_url)
 
-    def get_filename(self, url, index=''):
+    def get_filename(self, url: str, index=''):
         return (str(self.cls.item.author) + '_' + self.cls.item.id + index +
                 self.get_file_extension(url))
 
-    def get_file_extension(self, url):
+    def get_file_extension(self, url: str):
         parsed = urlparse(url)
         _, ext = os.path.splitext(parsed.path)
         return ext
-
-
-
 
 
 if __name__ == '__main__':

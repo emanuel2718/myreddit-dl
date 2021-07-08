63ecc9227a5fe701d1e6d6cbefdd4e2fa079f24c
emanuel2718 <eramirez2718@gmail.com>
2021-07-08 13:38:53 -0400

    added change client and change prefix cli interaction

--- myreddit-dl/cli.py
+++ myreddit-dl/cli.py
@@ -8,7 +8,6 @@ class Cli:
     def __init__(self):
         self.log = utils.setup_logger(__name__)
         self.config = ConfigHandler()
-        self.client = RedditClient()
 
     def display_setup_header(self) -> None:
         instructions = utils.DEVELOPER_APP_INSTRUCTIONS
@@ -40,7 +39,7 @@ class Cli:
                 'username': username,
                 'password': password}
 
-    def client_setup(self) -> None:
+    def client_setup(self, request=None) -> None:
         ''' Sets up new Reddit client if the given credentials are valid
             Reddit instances. If valid, then go ahead an add the client.
             If invalid; exit the program.
@@ -48,56 +47,42 @@ class Cli:
         self.display_setup_header()
         new_client_instance = self.new_client_prompt()
 
-        if self.client.validate_instance(new_client_instance):
-            self.config.add_client(instance)
+        if RedditClient.validate_instance(new_client_instance):
+            self.config.add_client(new_client_instance)
         else:
             self.log.warning('Invalid reddit instance provided')
             exit(0)
 
-    def change_client(self):
+    def change_client_prompt(self, options: dict) -> None:
+        print('\n Valid Reddit Clients:\n')
+        for k, v in options.items():
+            print(f'{k}. {v}')
+
+        res = ''
+        while res not in options.keys():
+            res = input('\nPlease, chose a client to change to: ')
+
+        if res == str(len(options)):
+            self.log.info('Thank you for using myreddit-dl')
+            exit(0)
+        self.config.set_new_current_user(options.get(res))
+
+    def get_clients_options(self) -> dict:
+        clients = self.config.get_available_reddit_clients()
+        clients.append('Exit program')
+        return {str(index):client for index, client in enumerate(clients, 1)}
+
+    def change_client(self, request=None):
+        options = self.get_clients_options()
+        self.change_client_prompt(options)
+
+    def change_path(self, request: str):
         pass
 
-    def show_config(self):
-        pass
-
-    def change_path(self):
-        pass
-
-    def change_prefix(self):
-        pass
+    def change_prefix(self, request: list):
+        self.config.set_prefix_option('_'.join(request))
 
 
-
-#
-#    def change_client(self):
-#        invalid_sections = {'DEFAULTS', 'USERS', 'REDDIT'}
-#        sections = [sec for sec in self.config.sections()
-#                    if sec not in invalid_sections]
-#        options = {}
-#        res = ''
-#        print('\nValid Reddit clients:\n')
-#        for i, section in enumerate(sections, 1):
-#            print(f'{i}. {self.config[section]["username"]}')
-#            options[str(i)] = self.config[section]['username']
-#        options[str(len(sections) + 1)] = 'Exit'
-#        print(f'{len(sections)+1}. Exit Program')
-#
-#        while res not in list(options.keys()):
-#            res = input('\nPlease chose the client you want to change to: ')
-#
-#        if res == str(len(sections) + 1):
-#            exit(0)
-#
-#        if self.config['USERS']['current_user_section_name'] == options[res].upper():
-#            self.log.info(
-#                f'{options[res]} is already the current reddit client.')
-#            exit(0)
-#
-#        self.config['USERS']['current_user_section_name'] = options[res].upper()
-#        with open(utils.CFG_FILENAME, 'w') as config_file:
-#            self.config.write(config_file)
-#            self.log.info(f'Reddit client changed to {options[res]}')
-#
 #    def _prompt_user_change(self, username: str) -> str:
 #        res = ''
 #        while res not in {'y', 'yes', 'n', 'no'}:
@@ -107,44 +92,3 @@ class Cli:
 #                ' (y)es, (n)o: ').lower()
 #        return res
 #
-#    def _config_setup_header(self):
-#        instructions = utils.DEVELOPER_APP_INSTRUCTIONS
-#        header = '-' * 20 + ' SETUP CONFIGURATOR ' + '-' * 20
-#        print('\n' + header)
-#        print(
-#            'For information'
-#            ' on how to setup your own developer credentials to use myreddit-dl,\n'
-#            'Please refer to',
-#            instructions)
-#
-#        print(
-#            '\nInput your reddit developer credentials below. Make sure they are correct.\n')
-#        print('-' * len(header))
-#
-#    def prompt_client_config_setup(self, hidden_password=False):
-#        print()
-#        utils.print_warning(
-#            'Either this is your first time using myreddit-dl'
-#            ' or there appear to be a problem in your reddit app'
-#            ' client information.\n\n')
-#        response = ''
-#        while response not in {'y', 'yes', 'n', 'no'}:
-#            response = input('Would you like to fix it? (y)es, (n)o: ').lower()
-#
-#        if response == 'n' or response == 'no':
-#            exit(0)
-#        return self.client_config_setup(hidden_password)
-#
-#    def print_config_data(self, show_password=False):
-#        print('Current Configuration:')
-#
-#        for section in self.config.sections():
-#            print(f'\n\n[{section}]')
-#            for (key, val) in dict(self.config[section]).items():
-#                if key == 'password':
-#                    if show_password:
-#                        print(f'{key.upper()} = {val}')
-#                    else:
-#                        continue
-#                else:
-#                    print(f'{key.upper()} = {val}')

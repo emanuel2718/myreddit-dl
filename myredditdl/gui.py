b444e5df17d87f45eadb89a8a609bb410aa49e1b
emanuel2718 <eramirez2718@gmail.com>
2021-07-12 16:38:32 -0400

    remove old cli file import

--- myredditdl/gui.py
+++ myredditdl/gui.py
@@ -1,9 +1,6 @@
-import utils
-
-
 def run_gui():
     print("Printing from gui_launcher.py!!!!")
 
 
 if __name__ == '__main__':
-    utils.print_warning(cli.DONT_RUN_THIS_FILE)
+    print('dont run this')
